ed57fdba56da483b4603107829bbe15548059087
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-04-21 12:46:23 -0400

    Removed unused files and submodules

--- utils/InstallDependencies.bat
+++
@@ -1,5 +0,0 @@
-.\libs\vcpkg\vcpkg install sdl2[vulkan]:x64-windows
-.\libs\vcpkg\vcpkg install boost-circular-buffer:x64-windows
-.\libs\vcpkg\vcpkg install xerces-c:x64-windows
-.\libs\vcpkg\vcpkg install curlpp:x64-windows
-.\libs\vcpkg\vcpkg install fmt:x64-windows
