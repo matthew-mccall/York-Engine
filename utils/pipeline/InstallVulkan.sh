15c558459cb6daa8d49202a0097ee847801cc278
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-04-21 16:09:42 -0400

    Learned Bash

--- utils/pipeline/InstallVulkan.sh
+++ utils/pipeline/InstallVulkan.sh
@@ -3,7 +3,7 @@
 pattern="1*"
 dirs=( $pattern )
 
-if [! -d "${dirs[0]}" ]; then
+if [ ! -d "${dirs[0]}" ]; then
   curl -fsSLO https://sdk.lunarg.com/sdk/download/latest/linux/vulkan-sdk.tar.gz
   tar xvf vulkan-sdk*
 fi
